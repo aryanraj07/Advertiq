c3f2450d7a15406357e146638c28dcc143645abe
aryanraj07 <aryanraj010010@gmail.com>
2024-08-14 17:48:24 +0530

    added a layout folder for header and footer

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,4 @@
 export { default as Home } from "./home/Home.jsx";
-export { default as Header } from "./header/Header.jsx";
-export { default as Footer } from "./footer/Footer.jsx";
 export { default as Services } from "./services/Services.jsx";
 export { default as About } from "./about/About.jsx";
 export { default as Contact } from "./contact/Contact.jsx";
